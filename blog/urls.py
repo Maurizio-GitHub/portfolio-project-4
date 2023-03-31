0e97ca50f11f765d5d805076b4fbf3f62f225fd1
Maurizio <maurizio.loffredo@outlook.com>
2023-03-31 19:29:38 +0000

    Feat: added view for post details

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 # When using class-based views, we need to add the 'as_view' method at the end
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
-# Class-based view for home page
+# Class-based view to render the home page
 class PostList(generic.ListView):
 
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 9
+
+
+# Class-based view to render in-full each specific post
+class PostDetail(View):
+
+    # Definition of our get-method
+    def get(self, request, slug, *args, **kwargs):
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by("created_on")
+        liked = False
+
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post-detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked
+            },
+        )
